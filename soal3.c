c717968e65d6d2b5920377e17ed58742c1504114
Unknown <rahandi16@mhs.if.its.ac.id>
2017-10-24 19:12:45 +0700

    penyelesaian fungsi main (belum dicoba) | penambahan break di setiap fungsi | memperbaiki typo

--- soal3.c
+++ soal3.c
@@ -3,26 +3,28 @@
 #include <pthread.h>
 #include <unistd.h>
 
-pthread_t thread_lohan, tread_kepiting;
+pthread_t thread_lohan, thread_kepiting, thread_input;
 int status_lohan=100, status_kepiting=100, signal=0;
 
 void* fungsi_lohan(){
 	while(1){
+		sleep(10)
 		status_lohan-=15;
 		if(status_lohan > 100 || status_lohan <= 0){
-			signal=1;
+			signal = 1;
+			break;
 		}
-		sleep(10)
 	}
 }
 
 void* fungsi_kepiting(){
 	while(1){
+		sleep(20)
 		status_kepiting-=10;
 		if(status_kepiting > 100 || status_kepiting <= 0){
 			signal = 1;
+			break;
 		}
-		sleep(20)
 	}
 }
 
@@ -36,17 +38,33 @@ void* ambil_input(){
 			status_lohan += 10;
 			if(status_lohan > 100 || status_lohan <= 0){
 				signal = 1;
+				break;
 			}
 		}
 		else if(pilihan == 2){
 			status_kepiting += 10;
 			if(status_kepiting > 100 || status_kepiting <= 0){
 				signal = 1;
+				break;
 			}
 		}
 	}
 }
 
 int main(){
+	system("clear");
+	pthread_create(&(thread_lohan), NULL, &fungsi_lohan, NULL);
+	pthread_create(&(thread_kepiting), NULL, &fungsi_kepiting, NULL);
+	pthread_create(&(thread_input), NULL, &ambil_input, NULL);
 
+	while(1){
+		if(signal==1){
+			printf("GAME OVER");
+			break;
+		}
+	}
+
+	pthread_join(thread_lohan, NULL);
+	pthread_join(thread_kepiting, NULL);
+	pthread_join(thread_input, NULL);
 }
