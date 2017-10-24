7584566cada766480ab601ce3e0153d6de467406
Unknown <rahandi16@mhs.if.its.ac.id>
2017-10-24 18:53:22 +0700

    persiapan variable | penyelesaian fungsi ambil input (belum dicoba)

---
+++ soal3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+pthread_t thread_lohan, tread_kepiting;
+int status_lohan=100, status_kepiting=100, signal=0;
+
+void* fungsi_lohan(){
+
+}
+
+void* fungsi_kepiting(){
+
+}
+
+void* ambil_input(){
+	int pilihan;
+	while(1){
+		printf("1.Beri makan lohan\n2.Beri makan kepiting\npilihan = ");
+		scanf("%d", &pilihan);
+		system("clear")
+		if(pilihan == 1){
+			status_lohan += 10;
+			if(status_lohan > 100 || status_lohan <= 0){
+				signal = 1;
+			}
+		}
+		else if(pilihan == 2){
+			status_kepiting += 10;
+			if(status_kepiting > 100 || status_kepiting <= 0){
+				signal = 1;
+			}
+		}
+	}
+}
+
+int main(){
+
+}
