c3b68689e4f0eeae9d585e7506f438d5fb996c7f
Unknown <rahandi16@mhs.if.its.ac.id>
2017-10-25 13:35:13 +0700

    soal 1 selesai semua

--- soal1 (pembeli).c
+++ soal1 (pembeli).c
@@ -16,7 +16,7 @@ int main(){
 		int pilihan, a, jumlah;
 		char senjatanya[8];
 		system("clear");
-		printf("1.Tampilkan stock senjata\n2.Tambah stock sejata\n");
+		printf("1.Tampilkan stock senjata\n2.Beli sejata\n3.Exit\n");
 		scanf("%d", &pilihan);
 		if(pilihan == 1){
 			system("clear");
@@ -29,16 +29,30 @@ int main(){
 			getchar();
 			getchar();
 		}
-		if(pilihan == 2){
+		else if(pilihan == 2){
 			system("clear");
 			printf("Format input [Nama barang] [Jumlah stok]\n");
 			scanf("%s %d", senjatanya, &jumlah);
 			for(a=0;a<6;a++){
 				if(strcmp(senjatanya, senjata[a]) == 0){
-					stok[a] += jumlah;
+					if(stok[a] < jumlah){
+						printf("Barang di stok tidak cukup\n");
+						printf("Press any key to continue...\n");
+						getchar();
+						getchar();
+						break;
+					}
+					stok[a] -= jumlah;
 					break;
 				}
 			}
+		}
+		else if(pilihan == 3){
+			system("clear");
+			printf("EXIT\n");
+			shmdt(stok);
+			shmctl(shmid, IPC_RMID, NULL);
+			break;
 		}
 	}
 	return 0;
