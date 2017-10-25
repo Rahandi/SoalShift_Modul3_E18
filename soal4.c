0a7260df451c8fc74658610d25bf04af0d2d24d3
Unknown <ghifarozarahmadiana@yahoo.com>
2017-10-25 08:29:08 +0700

    threading. finish

--- soal4.c
+++ soal4.c
@@ -3,56 +3,71 @@
 #include <pthread.h>
 #include <unistd.h>
 
-void fakto(int input_val[], int argc)
+pthread_t tid[100];
+int status = 0;
+
+typedef struct arg_struct{
+	int angka;
+	int stat;
+} args;
+
+void* fakto(void *arguments)
 {
-   int i, j;
-   //menghitung fakto
-   for (i=1; i<argc; i++)
-   {
-      int hasil = 1;
-      for (j=input_val[i]; j>1; j--)
-      {
-         hasil = hasil * j;
-      }
-      printf("%d! = %d\n",input_val[i], hasil);
-   }
+	int i, j;
+	struct arg_struct *args = (struct arg_struct *)arguments;
+	while(args->stat != status){}
+	int input_val = args->angka;
+	//menghitung fakto
+	int hasil = 1;
+	for (j=input_val; j>1; j--){
+		hasil = hasil * j;
+	}
+	printf("%d! = %d\n",input_val, hasil);
+	status++;
+	free(arguments);
 }
 
 
 int main(int argc, char *argv[])
 {
-   int i, j;
-   int input_val[100];   // integer version of argv[1]
-   int hasil; //hasil faktorial
-   
-   //cek ada argv apa g
-   if (argc < 2)
-   {
-      printf("INPUT TIDAK VALID\n");
-      exit(1);
-   }
+	int i, j;
+	int input_val[100];   // integer version of argv[1]
+	 
+	 //cek ada argv apa g
+	if (argc < 2){
+		printf("INPUT TIDAK VALID\n");
+		exit(1);
+	}
 
-   //convert string ke int
-   for (i=0; i<argc; i++)
-   {
-      input_val[i] = atoi(argv[i]);
-   }
+	 //convert string ke int
+	for (i=1; i<argc; i++){
+		input_val[i-1] = atoi(argv[i]);
+	}
 
-   //sorting
-   int temp;
-   for (i=1; i<argc; i++)
-   {
-      for (j=1; j<argc; j++)
-      {
-          if(input_val[j-1] >= input_val[j])                                
-          {
-             temp = input_val[j-1];                                    
-             input_val[j-1] = input_val[j];                                 
-             input_val[j] = temp;                                      
-          }    
-      }
-   }
+	//sorting
+	argc--;
+	int temp;
+	for (i=1; i<argc; i++){
+		for (j=1; j<argc; j++){
+			if(input_val[j-1] >= input_val[j]){
+	   			temp = input_val[j-1];                                    
+					input_val[j-1] = input_val[j];                                 
+					input_val[j] = temp;                                      
+			}    
+		}
+	}
 
-   fakto(input_val, argc);
+	for(i=0;i<argc;i++){
+		struct arg_struct *args = malloc(sizeof(struct arg_struct));
+		args->angka = input_val[i];
+		args->stat = i;
 
+		pthread_create(&tid[i], NULL, fakto, (void*)args);
+	}
+
+	for(i=0;i<argc;i++){
+		pthread_join(tid[i], NULL);
+	}
+
+	return 0;
 }
